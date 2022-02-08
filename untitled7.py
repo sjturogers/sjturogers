2913fb54f1a8e1e020805546655217eedf0d58af
sjturogers <98369376+sjturogers@users.noreply.github.com>
2022-02-08 10:25:08 +0800

    Add files via upload

---
+++ untitled7.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 10:49:36 2022
+
+@author: apple
+"""
+
+score=['chris',83,'david',96,'bill',92]
+for i in range(1,6,2):
+    print(score[i])
+    print(score[i-1])
---
+++ untitled8.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 11:34:10 2022
+
+@author: apple
+"""
+
+
+scores = [['chris', 83],
+         ['david', 96],
+         ['bill', 92],
+         ['amy', 97],
+         ['james', 77]]
+
+print(scores[1][0])
+         
