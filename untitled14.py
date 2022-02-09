ba7661db2e9d23fe3f4f2bdacdf1d00d1a3a52ad
sjturogers <98369376+sjturogers@users.noreply.github.com>
2022-02-09 10:46:18 +0800

    Add files via upload

---
+++ untitled14.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 10:39:31 2022
+
+@author: apple
+"""
+def 星：
+    對於 範圍內的i  ( 1，層+ 1 )：   
+        打印( '*'  *  i )
+        
+
+
+def  add_three ( num1 , num2 , num3 ):
+
+number  =  add_three ( 1 , 2 , 3 )
---
+++ untitled15.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 10:44:22 2022
+
+@author: apple
+"""
+
+numbers = [35, 67, 89, 97, 56, 88, 54]
+
+print(max(numbers))
+print(min(numbers))
+print(len(numbers))
+print(sorted(numbers))
+numbers = sorted(numbers)
+print(numbers)
+
+print(sum(numbers))
+
+names = ['rosa', 'primo', 'fang', 'edgar', 'bull']
+print(names)
